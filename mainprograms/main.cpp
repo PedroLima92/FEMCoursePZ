89e85fe796b9d9b514a6a4647f612048e749a7b4
PedroLima92 <pedrolima.ec@outlook.com>
2018-12-10 17:00:54 -0200

    december 10th

--- mainprograms/main.cpp
+++ mainprograms/main.cpp
@@ -11,10 +11,12 @@ using std::cin;
 
 int main ()
 {
-    cout<<"Polinomio:\n";
-    cout<<"     f(x) = 2 + x -x^2\n\n";
-    cout<<"Integrando de 0 a 0.8\n\n";
-    VecDouble f = {2,1,-1};
-    
+    double intnum;
+    cout << "Polinomio:\n";
+    cout << "     f(x) = 2 + x -x^2\n\n";
+    cout << "Integrando de 0 a 0.8\n\n";
+    VecDouble f = {2, 1, -1};
+    IntRule1d polinomio(3);
+    polinomio.Print();
     return 0;
 }
